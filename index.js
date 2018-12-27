6ec3e626682cf662bf7261cf838f6f9d54a1d948
Vong3432 <36991424+Vong3432@users.noreply.github.com>
2018-12-27 20:40:33 +0800

    .sidebar

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 $(window).scroll(function(){
 	parallax();
 	shrink();
-	openNav();
-	closeNav();
+	
 })
 
-function openNav()
-{
-	$(".top-nav").css("display","none");
-	$(".side-bar").css("display","flex");
-	$(".top-nav").addClass(".overlay");	
-	$(".overlay").css("display","block");
-}
 
-function closeNav()
-{
-	$(".top-nav").css("display","flex");
-	$(".side-bar").css("display","none");
-	$(".top-nav").removeClass(".overlay");
-	$(".overlay").css("display","none");	
-}
+// function openNav()
+// {
+// 	$(".top-nav").css("display","none");
+// 	$(".side-bar").css("display","flex");	
+// 	$(".top-nav").addClass(".overlay");	
+// 	$(".overlay").css("display","block");
+// }
+
+// function closeNav()
+// {	
+// 	$(".top-nav").css("display","flex");	
+//     $(".side-bar").css("display","none");   
+// 	$(".top-nav").removeClass(".overlay");
+//     $(".overlay").css("display","none");	
+// }
 
 function parallax()
 {
@@ -52,6 +52,26 @@ function shrink()
 
 	wPosition = w3Scroll;
 }
+
+$(document).ready(function()
+{	
+	$("#icon").click(function(){
+		$(".overlay").show();
+		$(".side-bar").animate({
+			"marginLeft":"-0px"
+		},1000);
+		$(".top-nav").hide();
+		console.log("marginLeft:0px")
+	});
+	$("#icon2").click(function(){
+		$(".overlay").hide();		
+		$(".side-bar").animate({
+			"marginLeft":"-600px",
+		});
+		$(".top-nav").show();
+		console.log("marginLeft:-500px")
+	});
+});
 
 $('.loader').hide();
 
